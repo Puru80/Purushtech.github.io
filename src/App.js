0a5d92dd2108b09c6f34c189f85dbb24161ae3a5
Puru80 <puru.agar99@gmail.com>
2023-10-26 14:46:26 +0530

    added HashRouter to support github pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.scss';
 import {HashRouter, Route, Routes} from "react-router-dom";
 import Layout from "./components/Layout";
+import Home from "./components/Home";
 
 function App() {
     return (
         <HashRouter>
             <Routes>
-                <Route path="/" element={<Layout/>}/>
+                <Route path="/" element={<Layout/>}>
+                    <Route index element={<Home/>}/>
+                </Route>
             </Routes>
         </HashRouter>
     );
