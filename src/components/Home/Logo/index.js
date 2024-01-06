238d928b7b7a1a79faecfc4cf2283b7b836456ea
Puru80 <puru.agar99@gmail.com>
2024-01-06 17:35:48 +0530

    removed gsap

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,15 +1,19 @@
-import LogoS from '../../../assets/images/p-logo-transparent.png'
 import './index.scss'
 
 const Logo = () => {
 
     return (
         <div className="logo-container" >
-            <img
-                className="solid-logo"
-                src={LogoS}
-                alt="Java,  Developer"
-            />
+            <svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">
+                {/* Outline */}
+                <rect width="100%" height="100%" fill="white" />
+                <rect x="10%" y="10%" width="80%" height="80%" fill="none" stroke="black" strokeWidth="5" />
+
+                {/* Filled Letter P */}
+                <text x="50%" y="50%" fontSize="40" textAnchor="middle" dy=".3em" fill="black">
+                    P
+                </text>
+            </svg>
 
         </div>
     )
